9e33a751f0d39390d4f5167357a8b8aff713fc84
Myroslav <myroslavtp@gmail.com>
2023-07-19 17:32:01 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="mainadd i">
+        <NuxtPage/>
+    </div>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+
+<style lang="scss">
+
+[class*='__container'] {
+    max-width: 1170px;
+    margin: 0 auto;
+    padding: 0 15px;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main__container">
+        <h3 class="text-5xl">Hello index</h3>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
