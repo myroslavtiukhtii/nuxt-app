a94c41e0562ebf350ea95dda5d170200bb0fb56c
Myroslav <myroslavtp@gmail.com>
2023-07-24 17:24:22 +0300

    dev

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="hero pt-32 -mt-28 relative">
+    <section class="hero pt-32 relative">
         <div class="hero__container
             grid grid-cols-1
             grid-rows-2 justify-items-center">
@@ -24,15 +24,31 @@
             </button>
         </div>
     </section>
-    <section class="model-3">
-        <picture class="model-3__picture" data-alt="Red Model 3 driving down a lakeside road" data-iesrc="../assets/images/Homepage-Model-3-Desktop-NA.avif">
+    <section class="models relative pt-32 pb-32">
+        <picture class="models__3__picture" data-alt="Red Model 3 driving down a lakeside road" data-iesrc="../assets/images/Homepage-Model-3-Desktop-NA.avif">
             <source srcset="../assets/images/Homepage-Model-3-Mobile-NA.avif" media="(max-width: 599px)">
             <source srcset="../assets/images/Homepage-Model-3-Desktop-NA.avif" media="(min-width: 600px)">
             <source srcset="../assets/images/Homepage-Model-3-Mobile-NA.avif" media="(min-width: 600px) and (orientation:portrait)">
-            <img class="model-3__image" src="../assets/images/Homepage-Model-3-Desktop-NA.avif" srcset="../assets/images/Homepage-Model-3-Desktop-NA.avif" alt="Red Model 3 driving down a lakeside road">
+            <img class="models__3__image
+            absolute w-full h-full 
+            object-cover top-0 
+            left-0 -z-10" src="../assets/images/Homepage-Model-3-Desktop-NA.avif" srcset="../assets/images/Homepage-Model-3-Desktop-NA.avif" alt="Red Model 3 driving down a lakeside road">
         </picture>
-        <div class="model-3__container">
-            
+        <div class="models__container
+            grid grid-cols-1
+            grid-rows-2 justify-items-center">
+            <div class="models__titles">
+                <h2 class="model__title text-black text-5xl mb-5">Model 3</h2>
+                <h3 class="model__subtitle tracking-wide">Starting at 
+                    <span class="model__price">$32,740</span>
+                </h3>
+                <span class="model__subtitle tracking-wide">After Federal Tax Credit</span>
+            </div>
+            <div class="models__buttons flex gap-5 flex-wrap justify-center self-center">
+                <button class="buttons__explore w-60 h-10 bg-white opacity-70 rounded-lg text-lg">Explore Inventory</button>
+                <button class="buttons__custom w-60 h-10 bg-black opacity-70 rounded-lg text-white text-lg
+                ">Custom Order</button>
+            </div>
         </div>
     </section>
 </template>
@@ -43,17 +59,26 @@
 
 <style lang="scss">
 
-
 [class*='__container'] {
     max-width: 1370px;
     margin: 0 auto;
     padding: 0 15px;
 }
 
-
-.hero__container {
+.hero__container, .models__container {
     min-height: 100vh;
 }
+
+.models {
+
+    &__titles {
+        text-align: center;
+    }
+
+}
+
+
+
 
 
 
