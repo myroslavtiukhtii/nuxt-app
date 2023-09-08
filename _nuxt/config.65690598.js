8b2c3b4e14a05962a2e7618d9977b2e673530874
Myroslav <myroslavtp@gmail.com>
2023-09-08 12:46:05 +0300

    commit

--- _nuxt/config.65690598.js
+++
@@ -1,1 +0,0 @@
-import{V as p,W as o,r as i}from"./entry.c914448e.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
---
+++ _nuxt/config.d7054a36.js
@@ -0,0 +1,1 @@
+import{V as p,W as o,r as i}from"./entry.5768165f.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
